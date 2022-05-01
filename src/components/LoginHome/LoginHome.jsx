71ad2092fd1bfa7ec467db61b8d84b9e00d136b7
Palak-shravagi <palakshravagi@gmail.com>
2022-05-01 17:35:30 +0530

    changes committed

--- src/components/LoginHome/LoginHome.jsx
+++ src/components/LoginHome/LoginHome.jsx
@@ -12,18 +12,21 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import { useNavigate } from "react-router-dom";
 import MenuItem from "@mui/material/MenuItem";
+import Rightsection from "../rightsection/Rightsection";
+import TextField from "@mui/material/TextField";
 // import AddIcon from '@material-ui/icons/Add';
 // import Info from "./components/Information/infromation";
-import Fab from '@mui/material/Fab';
-import logo from '../../final-logo-01.jpg';
-import { Row,Col } from "react-bootstrap";
-import Filter from "../filters/filter"
+import Fab from "@mui/material/Fab";
+import logo from "../../final-logo-01.jpg";
+import { Row, Col } from "react-bootstrap";
+import "./loginhome.css";
+import Filter from "../filters/filter";
 
 const pages = ["Home", "About us", "Contact"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function LoginHome() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -44,130 +47,184 @@ function LoginHome() {
   };
 
   return (
-      
     <>
-    <div className="gradient__bg">
-    <AppBar position="static" className="gradient__bg">
-    <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-            >
-              <img style={{width:"170px",height:"60px",marginTop:"5px",marginBottom:"5px",paddingBottom:"5px"}} src={logo}  />
-            </Typography>
+      <div style={{ backgroundColor: "rgb(13,17,23)" }}>
+        <AppBar
+          position="static"
+          className=""
+          style={{
+            backgroundColor: "#171A22",
+            marginBottom: "10px",
+          }}
+        >
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+              >
+                <img
+                  style={{
+                    width: "170px",
+                    height: "60px",
+                    marginTop: "5px",
+                    marginBottom: "5px",
+                    paddingBottom: "5px",
+                  }}
+                  src={logo}
+                />
+              </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            >
-              <img style={{width:"170px",height:"60px",marginTop:"5px",marginBottom:"5px",paddingBottom:"5px"}} src={logo}  />
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
                 >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  <MenuIcon />
                 </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
+                <img
+                  style={{
+                    width: "170px",
+                    height: "60px",
+                    marginTop: "5px",
+                    marginBottom: "5px",
+                    paddingBottom: "5px",
+                  }}
+                  src={logo}
+                />
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
                 ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      
-     </AppBar>
-       <Row>
-         <Col md={3} >
-         <Box width={20} component="span" m={1} >
-          <div width="20">
-            
-            <Col>
-            
-            <Button style={{marginLeft:"200px"}} onClick={() => navigate("/info")} variant="contained" color="primary" >Add Project</Button>
-            </Col>
-            </div>          
+              </Box>
 
-        </Box>
-         </Col>
-         <Col md={6}>
-         </Col>
-         <Col md={3 }>
-               <Filter/>
-         </Col>
-       </Row>
-      {/* <Box width={20} component="span" m={1} >
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <Row
+          style={{
+            backgroundColor: "rgb(13,17,23)",
+            paddingLeft: "50px",
+            paddingRight: "50px",
+          }}
+        >
+          <Col md={2}>
+            {/* <Box width={20} component="span" m={1}>
+              <div width="20">
+                <Col>
+                  <Button
+                    style={{ marginLeft: "200px" }}
+                    onClick={() => navigate("/info")}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Add Project
+                  </Button>
+                </Col>
+              </div>
+            </Box> */}
+
+            <Rightsection></Rightsection>
+          </Col>
+          {/* section in between the porject and filter tab */}
+          <Col md={7}>
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                style={{ backgroundColor: "rgb(13,17,23)" }}
+                class="form-control input-text"
+                placeholder="Type here to search"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-lg" type="button">
+                  <i class="fa fa-search"></i>
+                </button>{" "}
+              </div>
+            </div>
+          </Col>
+          <Col md={3} style={{ paddingRight: "0px" }}>
+            <Filter />
+          </Col>
+        </Row>
+        {/* <Box width={20} component="span" m={1} >
           <div width="20">
             <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             <Button onClick={() => navigate("/info")} variant="contained" color="primary" >Add Project</Button>
@@ -175,12 +232,11 @@ function LoginHome() {
             </div>          
 
         </Box> */}
-       
+
         {/* <Container maxWidth="sm"  >
             
         </Container> */}
-
-</div> 
+      </div>
     </>
   );
 }
---
+++ src/components/rightsection/Rightsection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Rightsection() {
+  return (
+    <>
+      <title>Glassmorphism login Form Tutorial in html css</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap"
+        rel="stylesheet"
+      />
+
+      {}
+      {/* <div className="gradient__bg">
+        <div className="shape" />
+        <div className="shape" />
+      </div > */}
+      <div
+        style={{
+          color: "#ffffff",
+          border: "1px solid rgb(138,149,158)",
+          borderRadius: "5px",
+          paddingLeft: "0px",
+          paddingRight: "0px",
+        }}
+      >
+        <div
+          style={{
+            padding: "10px",
+            backgroundColor: "rgb(23,26,34)",
+          }}
+        >
+          <img
+            src="E:\updated_project\project_hub\src\components\rightsection\img.jpg"
+            alt="img"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Rightsection;
